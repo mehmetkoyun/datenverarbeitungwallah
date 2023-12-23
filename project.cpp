7a2709ced290f0f53aa1c30083ac99eef4b8cc45
mehmetkoyun <152986471+mehmetkoyun@users.noreply.github.com>
2023-12-23 12:52:31 +0100

    Update project.cpp
    deltacompatibility eingefügt

--- project.cpp
+++ project.cpp
@@ -7,33 +7,55 @@
 #include <ctime>
 using namespace std;
 
-//liest die Sitzordnung aus der Datei ein und speichert sie in seating
-void readSeating(istream& input, vector<vector<int>>& seating) {
-	for(int i = 0; i < 10; ++i) {
-		string line;
-		getline(input, line);
-		istringstream iss(line);
-		for(int j = 0; j < 10; ++j) {
-			iss >> seating[i][j];
+const int rows = 10;
+const int cols = 10;
+
+vector<vector<int>> compatibilityMatrix = {
+	{10, 3, 0},
+	{3, 7, 9},
+	{0, 9, 2}};
+
+//liest eine Matrix aus einem Inputstream ein und speichert sie in einer Array ab
+void readMatrix(istream& input, vector<vector<int>>& matrix) {
+	for(int i = 0; i < rows; ++i) {
+		for(int j = 0; j < cols; ++j) {
+			input >> matrix[i][j];
 		}
 	}
 }
 
 //berechnet die Kompatibilität der Sitzordnung
-int totalCompatibility(vector<vector<int>>& seating, vector<vector<int>>& compatibilityMatrix) {
+int totalCompatibility(vector<vector<int>>& matrix, vector<vector<int>>& compatibilityMatrix) {
 	int sum = 0;
 	for(int i = 0; i < 9; ++i) {
 		for(int j = 0; j < 9; ++j) {
-			sum += compatibilityMatrix[seating[i][j]-1][seating[i][j+1]-1];
-			sum += compatibilityMatrix[seating[i][j]-1][seating[i+1][j]-1];
+			sum += compatibilityMatrix[matrix[i][j]-1][matrix[i][j+1]-1];
+			sum += compatibilityMatrix[matrix[i][j]-1][matrix[i+1][j]-1];
 		}
 	}
 	//letzte Zeile und letzte Spalte
 	for (int  i = 0; i < 9; i++) {
-		sum += compatibilityMatrix[seating[9][i+1]-1][seating[9][i]-1];
-		sum += compatibilityMatrix[seating[i][9]-1][seating[i+1][9]-1];
+		sum += compatibilityMatrix[matrix[9][i+1]-1][matrix[9][i]-1];
+		sum += compatibilityMatrix[matrix[i][9]-1][matrix[i+1][9]-1];
 	}
 	
+	return sum;
+}
+
+int localCompatibility(vector<vector<int>>& matrix, vector<vector<int>>& compatibilityMatrix, int row, int col) {
+	int sum = 0;
+	if(row > 0) {
+		sum += compatibilityMatrix[matrix[row][col]-1][matrix[row-1][col]-1];
+	}
+	if(row < rows - 1) {
+		sum += compatibilityMatrix[matrix[row][col]-1][matrix[row+1][col]-1];
+	}
+	if(col > 0) {
+		sum += compatibilityMatrix[matrix[row][col]-1][matrix[row][col-1]-1];
+	}
+	if(col < cols - 1) {
+		sum += compatibilityMatrix[matrix[row][col]-1][matrix[row][col+1]-1];
+	}
 	return sum;
 }
 
@@ -48,67 +70,59 @@ double acceptanceProbability(int currentScore, int newScore, double temperature)
 
 //implementiert den Simulated Annealing Algorithmus
 
-void algorithm(vector<vector<int>>& seating, vector<vector<int>>& compatibilityMatrix) {
-	double temperature = 1e4;
-	double minTemperature = 1e-5;
-	double coolingRate = 1e-5;
+void algorithm(vector<vector<int>>& matrix, vector<vector<int>>& compatibilityMatrix) {
+	double temperature = 1e6;
+	double minTemperature = 1e-6;
+	double coolingRate = 1e-6;
 
-	int currentScore = totalCompatibility(seating, compatibilityMatrix);
-
-	// Set random seed based on current time
-	//srand(time(0));
+	int currentScore = totalCompatibility(matrix, compatibilityMatrix);
+	srand(time(0));
 
 	while(temperature > minTemperature) {
-		vector<vector<int>> newSeating = seating;
-		// Zufällige Zahlen generieren
-
 		int randomRow1 = rand() % 10;
 		int randomRow2 = rand() % 10;
 		int randomCol1 = rand() % 10;
 		int randomCol2 = rand() % 10;
 		
-		int temp = newSeating[randomRow1][randomCol1]; //temporärer Speicher
-		newSeating[randomRow1][randomCol1] = newSeating[randomRow2][randomCol2]; //Tausch der Plätze von 1 durch zahl von 2
-		newSeating[randomRow2][randomCol2] = temp; //Tausch der Plätze von 2 durch zahl von 1
-		
-		
-		int newScore = totalCompatibility(newSeating, compatibilityMatrix);
-		
+		int deltaScore = 0;
+
+		deltaScore -= localCompatibility(matrix, compatibilityMatrix, randomRow1, randomCol1);
+		deltaScore -= localCompatibility(matrix, compatibilityMatrix, randomRow2, randomCol2);
+
+		swap(matrix[randomRow1][randomCol1], matrix[randomRow2][randomCol2]);
+
+		deltaScore += localCompatibility(matrix, compatibilityMatrix, randomRow1, randomCol1);
+		deltaScore += localCompatibility(matrix, compatibilityMatrix, randomRow2, randomCol2);
+
+		int newScore = currentScore + deltaScore;
+
 		if(acceptanceProbability(currentScore, newScore, temperature) > (double)rand() / (RAND_MAX)) {
-			seating = newSeating;
 			currentScore = newScore;
+		} else {
+			swap(matrix[randomRow1][randomCol1], matrix[randomRow2][randomCol2]);
 		}
 		
 		temperature *= 1 - coolingRate;
-	if (currentScore >= 1700) {
-		break;
-	}
 	}
 }
 
 int main() {
 
-	//Kompatibilitätsmatrix
-	vector<vector<int>> compatibilityMatrix = {
-		{10, 3, 0},
-		{3, 7, 9},
-		{0, 9, 2}};
+	vector<vector<int>> matrix(rows, vector<int>(cols));
 
-	//cin
-	vector<vector<int>> seating(10, vector<int>(10));
-	readSeating(cin, seating);
-	cout << "#Seating chart of size 10 by 10 with R = " << totalCompatibility(seating, compatibilityMatrix) << endl;
-	int sum = totalCompatibility(seating, compatibilityMatrix); // Pass compatibilityMatrix as a reference
-	algorithm(seating, compatibilityMatrix);
+	readMatrix(cin, matrix);
 
-	int optimizedSum = totalCompatibility(seating, compatibilityMatrix);
+	cout << "#Seating chart of size 10 by 10 with R = " << totalCompatibility(matrix, compatibilityMatrix) << endl;
+	int sum = totalCompatibility(matrix, compatibilityMatrix); // Pass compatibilityMatrix as a reference
+	algorithm(matrix, compatibilityMatrix);
+
+	int optimizedSum = totalCompatibility(matrix, compatibilityMatrix);
 	cout << "#Seating chart of size 10 by 10 with R = " << optimizedSum << endl;
 
-	for (const auto& row : seating) {
+	for (const auto& row : matrix) {
         for (int seat : row) {
             cout << seat << " ";
         }
         cout << endl;
     }
 }
-
